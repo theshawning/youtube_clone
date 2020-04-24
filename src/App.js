17ef7301a38b3343cf54fdfdc96441aa10d6080a
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-24 16:38:57 +0000

    refactored App and SearchBar to use React hooks instead of class based components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Grid } from '@material-ui/core';
 
@@ -7,17 +7,11 @@ import { SearchBar, VideoList, VideoDetail } from './components';
 import youtube from './api/youtube';
 import { apiKey } from './api/apikey';
 
-class App extends React.Component {
-  state = {
-    videos: [],
-    selectedVideo: null
-  }
+const App = () => {
+  const [ videos, setVideos ] = useState([]);
+  const [ selectedVideo, setSelectedVideo ] = useState(null);
 
-  onVideoSelect = video => {
-    this.setState({ selectedVideo: video});
-  }
-
-  handleSubmit = async searchTerm => {
+  const handleSubmit = async searchTerm => {
     const response = await youtube.get('search', { 
       params: {
         part: 'snippet',
@@ -27,32 +21,28 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ 
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-     })
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   }
 
-  render() {
-    const { selectedVideo, videos } = this.state;
-    return (
-      <Grid justify='center' container spacing={10}>
-        <Grid item xs={12}>
-          <Grid container spacing={10}>
-            <Grid item xs={12}>
-              <SearchBar onFormSubmit={this.handleSubmit} />
-            </Grid>
-            <Grid item xs={8}>
-              <VideoDetail video={selectedVideo} />
-            </Grid>
-            <Grid item xs={4}>
-              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
-            </Grid>
-          </Grid>        
-        </Grid>
+  return (
+    <Grid justify='center' container spacing={10}>
+      <Grid item xs={12}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onFormSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>        
       </Grid>
-    )
-  }
+    </Grid>
+  )
+
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,35 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Paper, TextField } from '@material-ui/core';
 
-class SearchBar extends React.Component {
-  state = {
-    searchTerm: ''
+const SearchBar = ({ onFormSubmit }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = e => setSearchTerm(e.target.value);
+
+  const onKeyPress = e => {
+    if(e.key === 'Enter') {
+      onFormSubmit(searchTerm)
+    }
   };
 
-  handleChange = (e) => this.setState({ searchTerm: e.target.value });
-
-  handleSubmit = (e) => {
-    const { searchTerm } = this.state;
-    const { onFormSubmit } = this.props;
-
-    onFormSubmit(searchTerm);
-
-    e.preventDefault();
-  };
-
-  render () {
-    return (
-      <Paper elevation={6} style={{ padding: '25px'}}>
-        <form onSubmit={this.handleSubmit}>
-          <TextField 
-            fullWidth 
-            label='Search...' 
-            onChange={this.handleChange} />
-        </form>
-      </Paper>
-    )
-  };
+  return (
+    <Paper elevation={6} style={{ padding: '25px'}}>
+        <TextField 
+          fullWidth 
+          label='Search...' 
+          value={searchTerm} 
+          onChange={handleChange} 
+          onKeyPress={onKeyPress} 
+        />
+    </Paper>
+  )
 };
 
 export default SearchBar;
